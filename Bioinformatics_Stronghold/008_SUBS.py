f77024762aedba5350f54c2c03d86c4500b3e50c
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-05 14:18:54 +0900

    Update 008_SUBS.py

--- Bioinformatics_Stronghold/008_SUBS.py
+++ Bioinformatics_Stronghold/008_SUBS.py
@@ -13,3 +13,23 @@ ATAT
 # Sample Output: "2 4 10"
 
 # url: https://rosalind.info/problems/FIB/
+
+
+def motif_location(seq, motif):
+    location = []
+    for i in range(len(seq)):
+        if seq[i] == motif[0]:
+            n = 0
+            for j in range(len(motif)):                
+                if i+j >= len(seq): break
+                if seq[i+j] == motif[j]: n += 1
+            if n == len(motif):
+                location.append(i+1)
+    return location
+
+with open("./rosalind_subs.txt", "r") as f:
+     seq, motif = f.read().rstrip().split('\n')
+
+location = motif_location(seq, motif)
+for loci in location:
+    print(loci, end=' ')
