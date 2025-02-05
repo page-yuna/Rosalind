2b47712f1a2eed0419ba3216425cae9106dddad9
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-05 15:07:23 +0900

    Update 008_PROT.py

--- Bioinformatics_Stronghold/008_PROT.py
+++ Bioinformatics_Stronghold/008_PROT.py
@@ -9,3 +9,11 @@
 # Sample Output: 
 
 # url: https://rosalind.info/problems/PROT/
+
+
+from Bio.Seq import Seq
+
+with open("./rosalind_prot.txt", "r") as f:
+     rna_seq = Seq(f.read().rstrip())
+protein = rna_seq.translate(to_stop=True)
+print(protein)
