56fe8dda18de896d01b6f15df9fbd7278fe03997
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-05 13:45:36 +0900

    Create 008_ SUBS.py

---
+++ Bioinformatics_Stronghold/008_ SUBS.py
@@ -0,0 +1,15 @@
+### Finding a Motif in DNA
+# ID: SUBS
+# Topic: String Algorithms
+
+# Given: Two DNA strings s and t (each of length at most 1 kbp).
+# Return: All locations of t as a substring of s.
+
+# Sample Dataset:
+"""
+GATATATGCATATACTT
+ATAT
+"""
+# Sample Output: "2 4 10"
+
+# url: https://rosalind.info/problems/FIB/
