5587718b55fd5bfa7db4ab25d7d92777c349a921
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-06 10:53:38 +0900

    Update 010_CONS(2).py

--- Bioinformatics_Stronghold/010_CONS(2).py
+++ Bioinformatics_Stronghold/010_CONS(2).py
@@ -35,4 +35,49 @@ T: 1 5 0 0 0 1 1 6
 # url: https://rosalind.info/problems/cons/
 
 
-profile을 array로 풀어보기
+import numpy as np
+from collections import Counter
+
+def read_fasta(fasta_str):
+    sequences, seq = {}, ''
+    for line in fasta_str.strip().split('\n'):
+        if line.startswith('>'):
+            if seq:
+                sequences[name] = seq
+                seq = ''
+            name = line[1:]
+        else: seq += line.strip()
+    if seq:
+        sequences[name] = seq
+    return sequences
+
+
+def profile(sequences):
+    seq_list = list(sequences.values())
+
+    seq_array = np.array([list(seq) for seq in seq_list])
+    profiles = [Counter(seq_array[:, i]) for i in range(seq_array.shape[1])]
+    nucleotides = ['A', 'C', 'G', 'T']
+    profile_matrix = np.array([[counter[nuc] for nuc in nucleotides] for counter in profiles]).T
+
+    max_indices = np.argmax(profile_matrix, axis=0)
+    consensus = ''.join([nucleotides[i] for i in max_indices])
+
+    return profile_matrix, consensus
+
+
+def main(fpath):
+    with open(fpath, 'r') as f:
+        fasta_f = f.read()
+
+    sequences = read_fasta(fasta_f)
+    profile_matrix, consensus = profile(sequences)
+    print(consensus)
+
+    for i, nu in enumerate(['A', 'C', 'G', 'T']):
+        print(f"{nu}: {' '.join(map(str, profile_matrix[i]))}")
+
+
+if __name__ == "__main__":
+    fpath = "./rosalind_cons.txt"
+    main(fpath)
