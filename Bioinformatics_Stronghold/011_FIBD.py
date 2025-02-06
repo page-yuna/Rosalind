d1f7c4d00f531e5fb3c5ddbcebe04075ff53c3a9
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-06 12:42:26 +0900

    Create 011_FIBD.py

---
+++ Bioinformatics_Stronghold/011_FIBD.py
@@ -0,0 +1,13 @@
+### Mortal Fibonacci Rabbits
+# ID: FIBD
+# Topic: Combinatorics, Dynamic Programming
+
+# Given: Positive integers n≤100 and m≤20.
+# Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
+
+# Sample Dataset: "6 3"
+# Sample Output: "4"
+
+# url: https://rosalind.info/problems/fibd/
+
+
