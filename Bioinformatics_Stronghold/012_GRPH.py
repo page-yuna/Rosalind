668deca3d48d062648fb9a5cb34855a80fd892dc
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-06 15:36:31 +0900

    Update 012_GRPH.py

--- Bioinformatics_Stronghold/012_GRPH.py
+++ Bioinformatics_Stronghold/012_GRPH.py
@@ -27,9 +27,35 @@ Rosalind_2391 Rosalind_2323
 
 # url: https://rosalind.info/problems/grph/
 
-def ...
+def read_fasta(fasta_str):
+    sequences, seq = {}, ''
+    for line in fasta_str.strip().split('\n'):
+        if line.startswith('>'):
+            if seq:
+                sequences[name] = seq
+                seq = ''
+            name = line[1:]
+        else: seq += line.strip()
+    if seq:
+        sequences[name] = seq
+    return sequences
 
+def overlap(seq_dic):
+    overlap = []
+    for name1 in seq_dic:
+        for name2 in seq_dic:
+            if name1 != name2:
+                if seq_dic[name1][-3:] == seq_dic[name2][:3]:
+                    pair = (name1, name2)
+                    overlap.append(pair)
+                    
+    return overlap
+
+fpath = "./rosalind_grph.txt"
 with open(fpath, 'r') as f:
     fasta_f = f.read()
 
-sequences = read_fasta(fasta_f)
+    sequences = read_fasta(fasta_f)
+    ol_list = overlap(sequences)
+    for ol in ol_list:
+        print(ol[0], ol[1])
