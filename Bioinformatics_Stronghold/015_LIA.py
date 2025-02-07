5b599cddf1333fba5005188f41ba3ce254d476c0
먉 <128760373+page-yuna@users.noreply.github.com>
2025-02-07 17:10:30 +0900

    Create 015_LIA.py

---
+++ Bioinformatics_Stronghold/015_LIA.py
@@ -0,0 +1,12 @@
+### Independent Alleles
+# ID: LIA
+# Topics: Heredity, Probability
+
+# Given: Two positive integers k (k≤7) and N (N≤2k). In this problem, we begin with Tom,
+#        who in the 0th generation has genotype Aa Bb. Tom has two children in the 1st generation,
+#        each of whom has two children, and so on. Each organism always mates with an organism having genotype Aa Bb.
+# Return: The probability that at least N Aa Bb organisms will belong to the k-th generation of Tom's family tree
+#         (don't count the Aa Bb mates at each level). Assume that Mendel's second law holds for the factors.
+
+# Sample Dataset: "2 1"
+# Sample Output: "0.684"
